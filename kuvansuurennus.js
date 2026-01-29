db5421cc836889665cce36a2647945ce8234b441
Hannu Kukka <hannu.kukka@gmail.com>
2026-01-29 19:04:46 +0200

    Kuvat avautuvat nyt modaalissa

---
+++ kuvansuurennus.js
@@ -0,0 +1,14 @@
+// Kuvan suurennus-logiikka
+document.addEventListener('click', function (e) {
+    // Etsitään lähin .img-container klikatun kohdan ympäriltä
+    const container = e.target.closest('.img-container');
+    
+    if (container) {
+        const img = container.querySelector('img');
+        if (img) {
+            const src = img.getAttribute('src');
+            const modalImg = document.getElementById('modalImage');
+            modalImg.src = src;
+        }
+    }
+});
